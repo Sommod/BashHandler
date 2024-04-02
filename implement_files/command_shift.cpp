b8b22c284ee675b06b4b68cb84ec82aee9bf5e13
josmoor <josmoor@iu.edu>
2024-04-02 14:40:39 -0400

    Added command file

---
+++ implement_files/command_shift.cpp
@@ -0,0 +1,6 @@
+#include "../header_files/internal_commands.h" // String, Vector
+#include "../header_files/util.h" // String, Vector, Unistd.h
+
+bool internal_shift(std::string args) {
+    return false;
+}
---
+++ implement_files/command_source.cpp
@@ -0,0 +1,9 @@
+#include "../header_files/internal_commands.h" // String, Vector
+#include "../header_files/util.h" // String, Vector, Unistd.h
+
+// Example input >>>>> ("" are just for representation)     "source filename.sh [args...]"
+// This is the same as though the user inputted this command: ". filename.sh [args...]"
+// '.' and SOURCE are similar commands, if you need to research what 'source' does.
+bool internal_source(std::string args) {
+    return false;
+}
