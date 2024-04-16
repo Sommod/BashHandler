a3b4edde4ab40024f07ebb69a2ef6af6f1edd925
mavcolon <mavcolon@tesla.cs.iupui.edu>
2024-04-15 21:10:11 -0400

    fixing shift

--- implement_files/command_shift.cpp
+++ implement_files/command_shift.cpp
@@ -19,18 +19,15 @@ insied of the file
 */
 
 
-bool internal_shift(std::string args) {
+std::string internal_shift(std::string args) {
      std::vector<std::string> input = split_string(args," "); // this gets the input form user and  puts it into a vector [f1,f2,f3,2] 
      int N = input.size(); // gives me the total size of vector size/ total number of argumnets 4 
-      for (int i =0; i< N; i++){
-        int  n = input.at(i-1); // this gets the final argumnet form the vector
+      for (int i=0; i< N; i++){
+        std::vector<std::string> n (input.at(i),input.end()); // this gets the final argumnet form the vector [2]
         if( n ='1'){
             std::string newargs = input.at(N+1);
-            return true;
         }else{
-            std::string newargs = input.at(n);
+            //std::string newargs = input.at();
         }
-        
       }
-    return false;
 }
